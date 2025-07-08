1cb07242294bbf1fb71ef983dcbbff0583e3fdf4
Loren <laurentzi.sm@gmail.com>
2025-07-08 14:05:02 +0200

    Update SeleniumConfig.java

--- Testauto/src/main/java/com/sm/config/SeleniumConfig.java
+++ Testauto/src/main/java/com/sm/config/SeleniumConfig.java
@@ -10,9 +10,13 @@ import org.springframework.context.annotation.Configuration;
 public class SeleniumConfig {
 
 	@Bean
-	WebDriver webDriver() {
+	WebDriver webDriver() throws IOException {
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--headless");
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		String tempProfileDir = Files.createTempDirectory("chrome-profile-").toString();
+		options.addArguments("--user-data-dir=" + tempProfileDir);
 	    return new ChromeDriver();
 	}
 }
